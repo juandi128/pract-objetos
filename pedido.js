db8c35f053197a3445219cc61861779e1319d204
juandi128 <tuemail@ejemplo.com>
2025-08-22 17:19:22 -0500

    first commit

--- pedido.js
+++ pedido.js
@@ -3,11 +3,16 @@ import Producto from './model/Producto.js';
 import Factura from './model/Factura.js';
 
 const persona1 = new Persona(1, 'Mauricio', 'Rivero', '1984-01-02');
-const producto1 = new Producto(1,'Nike Jordan Air', '1500000');
-const producto2 = new Producto(2,'Nintendo Switch 2', '2000000');
-const producto3 = new Producto(3,'NB 2000 Red Bull', '370000');
+const producto1 = new Producto(1,'Nike Jordan Air', 1500000);
+const producto2 = new Producto(2,'Nintendo Switch 2', 2000000);
+const producto3 = new Producto(3,'NB 2000 Red Bull', 370000);
+const producto4 = new Producto(1,'Nike Air Force One', 700000);
+const producto5 = new Producto(2,'Apple Iphone 18 Pro Max', 12000000);
+const producto6 = new Producto(3,'Macbook M1', 2600000);
 
-let productos = [producto1, producto2, producto3];
+let productos = [producto1, producto2, producto3, producto4, producto5, producto6];
 
 const factura1 = new Factura(1, '2025-08-19', productos, persona1);
-console.log(factura1);
+
+factura1.calcularFactura(19);
+
